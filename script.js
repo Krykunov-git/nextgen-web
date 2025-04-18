0d1ebfe6220698ad8ce485872bfcf558053b1cbb
Ivan <honda27nn@gmail.com>
2025-04-18 11:14:24 +0200

    Survey finish

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const button = document.querySelector('.survey_box .survey_button');
 button.addEventListener('click', () => {
   button.closest('.survey_box').classList.toggle('active');
 });
+const buttons = document.querySelectorAll('.survey_punkt .survey_button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.closest('.survey_punkt').classList.toggle('active');
+  });
+});
